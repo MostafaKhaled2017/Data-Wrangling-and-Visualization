4ad493a4b3ae415b28f5af6ad9a21a22826fbd29
mosta <mostafakhaled835@outlook.com>
2023-07-20 00:12:00 +0300

    Updating Visualizations

--- scripts/d3Script.js
+++ scripts/d3Script.js
@@ -141,26 +141,84 @@ async function createMostExpensiveCurrenciesBarChart() {
   }
 
   
-// Function to create the pie chart showing top 5 cryptocurrencies by volume
-async function createTop5CryptoByVolumePieChart() {
-  const data = (await fetchData())
+// Function to create the pie chart showing top 3 cryptocurrencies by volume
+async function createTop3CryptoByVolumePieChart() {
+  var data = await fetchData()
 
-  const top_5data = data.slice(0, 5);
+  const top_3data = data.slice(0, 3);
+  console.log(top_3data)
+  // Calculate the total volume of other cryptocurrencies (excluding the top 3)
+  const other_volume = data.slice(3).reduce((sum, currency) => sum + currency.volume, 0);
 
-  // Calculate the total volume of other cryptocurrencies (excluding the top 5)
-  const other_volume = data.slice(5).reduce((sum, currency) => sum + currency.volume, 0);
-
-  // Create a new array for the pie chart data, including the top 5 and "Others"
+  // Create a new array for the pie chart data, including the top 3 and "Others"
   const pie_data = [
-    ...top_5data,
+    ...top_3data,
     { name: 'Others', volume: other_volume }
   ];
 
+  const data_dict = {};
+  pie_data.forEach((e) => {
+    data_dict[e.name] = e.volume;
+  });
 
+// set the dimensions and margins of the graph
+var width = 450,
+height = 450,
+margin = 40;
+
+// The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
+var radius = Math.min(width, height) / 2 - margin
+
+// append the svg object to the div
+var svg = d3.select("#top-3-crypto-volume")
+.append("svg")
+.attr("width", width)
+.attr("height", height)
+.append("g")
+.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+// set the color scale
+var color = d3.scaleOrdinal()
+  .domain(data_dict)
+  .range(d3.schemeSet2);
+
+// Compute the position of each group on the pie:
+var pie = d3.pie()
+  .value(function(d) {return d.value; })
+var data_ready = pie(d3.entries(data_dict))
+// Now I know that group A goes from 0 degrees to x degrees and so on.
+
+// shape helper to build arcs:
+var arcGenerator = d3.arc()
+  .innerRadius(0)
+  .outerRadius(radius)
+
+// Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+svg
+.selectAll('mySlices')
+.data(data_ready)
+.enter()
+.append('path')
+.attr('d', arcGenerator)
+.attr('fill', function(d){ return(color(d.data.key)) })
+.attr("stroke", "black")
+.style("stroke-width", "2px")
+.style("opacity", 0.7)
+
+// Now add the annotation. Use the centroid method to get the best coordinates
+svg
+.selectAll('mySlices')
+.data(data_ready)
+.enter()
+.append('text')
+.text(function(d){ return d.data.key})
+.attr("transform", function(d) { return "translate(" + arcGenerator.centroid(d) + ")";  })
+.style("text-anchor", "middle")
+.style("font-size", 17)
 
 }
   
   // Call the functions to create the plots
   createTopCurrenciesPriceBarChart();
   createMostExpensiveCurrenciesBarChart();
-  createTop5CryptoByVolumePieChart();  
+  createTop3CryptoByVolumePieChart();  
--- scripts/plotlyScript.js
+++ scripts/plotlyScript.js
@@ -24,7 +24,7 @@ async function createTop5CryptoPricePlot() {
   };
 
   const layout = {
-    title: 'Top 5 Cryptocurrencies Price',
+    title: 'Price of The Top 5 Cryptocurrencies',
     yaxis: {
       title: 'Price (USD)',
     },
@@ -55,17 +55,18 @@ async function createMostExpensiveCryptoPlot() {
 }
   
 // Function to create the pie chart showing top 5 cryptocurrencies by volume
-async function createTop5CryptoByVolumePieChart() {
-    const data = (await fetchData())
+async function createTop3CryptoByVolumePieChart() {
+    const data = await fetchData()
 
-    const top_5data = data.slice(0, 5);
+    const top_3data = data.slice(0, 3);
 
-    // Calculate the total volume of other cryptocurrencies (excluding the top 5)
-    const other_volume = data.slice(5).reduce((sum, currency) => sum + currency.volume, 0);
-
-    // Create a new array for the pie chart data, including the top 5 and "Others"
+    // Calculate the total volume of other cryptocurrencies (excluding the top 3)
+    const other_volume = data.slice(3).reduce((sum, currency) => sum + currency.volume, 0);
+    
+    console.log(other_volume)
+    // Create a new array for the pie chart data, including the top 3 and "Others"
     const pie_data = [
-      ...top_5data,
+      ...top_3data,
       { name: 'Others', volume: other_volume }
     ];
 
@@ -77,14 +78,14 @@ async function createTop5CryptoByVolumePieChart() {
     };
         
     const layout = {
-        title: "Top 5 Crypto Volume",
+        title: "Volume of The Top 3 Cryptocurrencies",
     };
         
-    Plotly.newPlot('top-5-crypto-volume', [trace], layout);  
+    Plotly.newPlot('top-3-crypto-volume', [trace], layout);  
 }
   
 // Call the functions to create the plots
 createTop5CryptoPricePlot();
 createMostExpensiveCryptoPlot();
-createTop5CryptoByVolumePieChart();
+createTop3CryptoByVolumePieChart();
   
